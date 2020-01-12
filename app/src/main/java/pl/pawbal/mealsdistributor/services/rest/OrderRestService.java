e8713b2aed1c9ef6dcaeb50edb64765f1c1e1690
Pawel Balcerek <pawbal96@gmail.com>
2020-01-12 19:46:34 +0100

    REST, Wrapper, ErrorHandler, SuccessHandler - Order services
    Added:
    
    - OrderErrorHandler, OrderSuccessHandler:
      - for now empty classes
    - OrderRestService:
      - with methods: getOrder, getOrders, markOrdered
    - OrderWrapperService:
      - with methods: getOrder, getOrders, markOrdered [with implementation]
      - tests for implementation

---
+++ app/src/main/java/pl/pawbal/mealsdistributor/services/rest/OrderRestService.java
@@ -0,0 +1,25 @@
+package pl.pawbal.mealsdistributor.services.rest;
+
+import java.util.UUID;
+
+import io.reactivex.Single;
+import pl.pawbal.mealsdistributor.models.dto.response.order.GetOrder;
+import pl.pawbal.mealsdistributor.models.dto.response.order.GetOrders;
+import retrofit2.http.GET;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface OrderRestService {
+    String ORDERS_BASE_PATH = "/orders";
+    String ORDER_BASE_PATH = "/order";
+
+    @GET(ORDER_BASE_PATH + "/{id}")
+    Single<GetOrder> getOrder(@Path("id") UUID id);
+
+    @GET(ORDERS_BASE_PATH)
+    Single<GetOrders> getOrders();
+
+    //TODO: verify returns from single
+    @PUT(ORDER_BASE_PATH + "/{id}/ordered")
+    Single<Void> markOrdered(@Path("id") UUID id);
+}
