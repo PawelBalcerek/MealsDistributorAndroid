c07630cdab4fd93179097544f2909f694f54641b
Pawel Balcerek <pawbal96@gmail.com>
2020-01-13 19:20:51 +0100

    REST, Wrapper, ErrorHandler, SuccessHandler - OrderPropositionPosition service
    Added:
    
    - OrderPropositionPositionErrorHandler, OrderPropositionPositionSuccessHandler:
      - for now empty classes
    - OrderPropositionPositionRestService:
      - with methods: getOrderPropositionPositions, addOrderPropositionPosition
    - OrderPropositionPositionService, OrderPropositionPositionWrapperService:
      - with methods: getOrderPropositionPositions, addOrderPropositionPosition [with implementations]
      - tests for implementations

---
+++ app/src/main/java/pl/pawbal/mealsdistributor/services/actions/error/OrderPropositionPositionErrorHandler.java
@@ -0,0 +1,6 @@
+package pl.pawbal.mealsdistributor.services.actions.error;
+
+import pl.pawbal.mealsdistributor.services.actions.core.ErrorHandler;
+
+public class OrderPropositionPositionErrorHandler implements ErrorHandler {
+}
